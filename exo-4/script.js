230133e281752857eb0412faadd5b3c1c2fea2ed
Anthony <anthony.bellier@laposte.net>
2023-01-11 19:14:46 +0100

    Les tableaux

---
+++ exo-4/script.js
@@ -0,0 +1,70 @@
+// you can write js here
+
+console.log("exo-4");
+
+var secretMessage = [
+  "Learning",
+  "isn't",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it's",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
+
+// Etape 1 : Enlever le dernier élément du tableau
+
+secretMessage.pop();
+
+console.log(secretMessage);
+
+// Etape 2 : Ajouter des mots au tableau
+
+secretMessage.push("to", "program");
+
+console.log(secretMessage);
+
+// Etape 3 : Changer un mot du tableau
+
+secretMessage[6] = "right";
+
+console.log(secretMessage);
+
+// Etape 4 : Supprimer le premier élément du tableau
+
+secretMessage.shift();
+
+console.log(secretMessage);
+
+// Etape 5 : Ajouter une chaine au début du tableau
+
+secretMessage.unshift("Programming");
+
+console.log(secretMessage);
+
+// Etape 6 : Remplacer des mots par un seul mot
+
+secretMessage.splice(5, 5, "know");
+
+console.log(secretMessage);
+
+// Etape7 : Utilisation de la méthode .join() pour afficher le message secret
+
+secretMessage.join(" ");
+
+console.log(secretMessage);
