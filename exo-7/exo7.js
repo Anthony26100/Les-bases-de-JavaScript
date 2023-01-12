6f77eb78fe40588bf65f5060fdd73879258c8ada
Anthony26100 <anthony.bellier@le-campus-numerique.fr>
2023-01-12 16:24:30 +0100

    Listing articles in progress

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,25 @@
 console.log("exo-7");
 
+
+// afficher
+
+
+let traduction = {
+    car: "voiture",
+    house: "maison",
+    game: "jeu",
+    videoGame: "jeux vidéos",
+    show: "show"
+}
+
+let articles = document.querySelector('#article');
+
+jsonDatas.forEach((e) => {
+    // fait matcher les données avec l'objet traduction et type du fichier datas.js pour ajouter une donnée "trad" dans le fichier datas.js
+    e.trad = traduction[e.type];
+    articles.innerHTML += ('<div class="article">'+e.name+'</div>');
+});
 console.log(jsonDatas);
+
+let btn = document.querySelector("btn");
+let contenu = document.querySelector(".text");
