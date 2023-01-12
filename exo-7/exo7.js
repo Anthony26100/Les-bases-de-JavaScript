0a98fe6b6d1b1382b0c286455f5030583fb90364
Anthony <anthony.bellier@laposte.net>
2023-01-12 20:38:27 +0100

    exo-7 progress

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,25 +1,34 @@
 console.log("exo-7");
 
-
 // afficher
 
-
 let traduction = {
-    car: "voiture",
-    house: "maison",
-    game: "jeu",
-    videoGame: "jeux vidéos",
-    show: "show"
-}
+  car: "voiture",
+  house: "maison",
+  game: "jeu",
+  videoGame: "jeux vidéos",
+  show: "show",
+};
 
-let articles = document.querySelector('#article');
+let articles = document.querySelector("#article");
 
 jsonDatas.forEach((e) => {
-    // fait matcher les données avec l'objet traduction et type du fichier datas.js pour ajouter une donnée "trad" dans le fichier datas.js
-    e.trad = traduction[e.type];
-    articles.innerHTML += ('<div class="article">'+e.name+'</div>');
+  // fait matcher les données avec l'objet traduction et type du fichier datas.js pour ajouter une donnée "trad" dans le fichier datas.js
+  e.trad = traduction[e.type];
+  articles.innerHTML += '<div class="article">' + e.name + "</div>";
 });
 console.log(jsonDatas);
 
-let btn = document.querySelector("btn");
-let contenu = document.querySelector(".text");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", (e) => {
+  let sect = document.querySelector(".text");
+  let para = document.createElement("p");
+  para.textContent = traduction.videoGame;
+  para.textContent = jsonDatas[0].name;
+  sect.appendChild(para);
+  console.log(para);
+  console.log(jsonDatas);
+  //ontenu.innerHTML += `Articles :  ${e}`;
+  //console.log(contenu);
+});
